51a1ef9d14b5c7cf936c30f15f3df1d29086ce38
Gilbert <okonjogilbert2@gmail.com>
2023-02-03 20:55:59 +0300

    generate like model

---
+++ app/models/like.rb
@@ -0,0 +1,11 @@
+class Like < ApplicationRecord
+  belongs_to :post
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  after_save :like_counter
+
+  private
+
+  def like_counter
+    post.increment!(:likes_counter)
+  end
+end
