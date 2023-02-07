c28d94e5a380665f463162385ff08f02ff55cd60
Gilbert <okonjogilbert2@gmail.com>
2023-02-07 10:17:13 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
     resources :posts ,only:[:index,:show]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'users#index', as: 'home
+  root 'users#index', as: 'home'
 end
+
+
