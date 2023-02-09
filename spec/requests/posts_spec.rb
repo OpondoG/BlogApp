1f11b7262faff403bc65a82d8eb3d5e50468335b
Gilbert <okonjogilbert2@gmail.com>
2023-02-09 09:24:34 +0300

    Fix posts_spec tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -33,7 +33,7 @@ RSpec.describe 'Posts', type: :request do
     end
 
     it 'should include correct placeholder' do
-      expect(response.body).to include("Here is a single Post for a given user you are in '/users/user_id/posts/id")
+      expect(response.body).to include("Here is a single Post for a given user you are in '/users/user_id/posts'")
     end
   end
 end
