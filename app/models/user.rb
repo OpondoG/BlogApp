e15095e519ba25e534c9fb00f60f6e6c0f6389a8
Gilbert <okonjogilbert2@gmail.com>
2023-02-11 11:41:36 +0300

    Fix Rubocop linter

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
 
-
   validates :name, presence: true, length: { maximum: 250 }
   validates :posts_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true },
                             allow_nil: true
