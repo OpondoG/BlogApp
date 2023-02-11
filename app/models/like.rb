2938ae52780477e664a587e769dd419e99e92693
Gilbert <okonjogilbert2@gmail.com>
2023-02-11 17:24:51 +0300

    Created the likes functionality

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,6 +3,10 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   after_save :like_counter
 
+  after_save :update_likes_counter
+
+  def update_likes_counter; end
+
   private
 
   def like_counter
