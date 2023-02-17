2dd83a3406e1c0316e38cbb76af847f4e0ddbf22
Gilbert <okonjogilbert2@gmail.com>
2023-02-17 19:13:40 +0300

    Permitted additional columns to devise when Signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   def current_post
     User.first.posts.first
+  end
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :bio, :posts_counter, :photo])
   end
 end
